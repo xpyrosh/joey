5b78a8dec9f01425c92ef7ea3fae696038332d09
Shiva Ramsamooj <shiva.ramsamooj@hotmail.com>
2019-04-07 18:58:41 -0400

    intial commit

---
+++ polls/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    # Ex /polls/
+    path('', views.index, name='index'),
+    # Ex /polls/5
+    path('<int:question_id>/', views.detail, name='detail'),
+    # Ex /polls/5/results
+    path('<int:question_id>/results/', views.results, name='results'),
+    # Ex /polls/5/vote
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+]
