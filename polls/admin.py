d699fd2257c0736c7903d0b547cd3efb78b3f80a
Shiva Ramsamooj <shiva.ramsamooj@hotmail.com>
2019-04-10 00:08:56 -0400

    made changes to how choices are added/modified on the admin page by attaching them to their respective questions edit page

--- polls/admin.py
+++ polls/admin.py
@@ -5,11 +5,17 @@ from .models import Question, Choice
 
 # Register your models here.
 
+class ChoiceInLine(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None,                {'fields': ['question_text']}),
         ('Date Information:', {'fields': ['pub_date']})
     ]
+    inlines = [ChoiceInLine]
 
 
 admin.site.register(Question, QuestionAdmin)
