f8c4da31e73e217f1b11f6a680c1dc715e9aa464
Shiva Ramsamooj <shiva.ramsamooj@hotmail.com>
2019-04-10 00:18:59 -0400

    added search function to admins page for questions

--- polls/admin.py
+++ polls/admin.py
@@ -18,6 +18,7 @@ class QuestionAdmin(admin.ModelAdmin):
     inlines = [ChoiceInLine]
     list_display = ('question_text', 'pub_date', 'was_published_recently')
     list_filter = ['pub_date']
+    search_fields = ['question_text']
 
 
 admin.site.register(Question, QuestionAdmin)
