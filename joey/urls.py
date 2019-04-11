ad89c8efa87a447ad731c510fb72243e3fb1fa5a
Shiva Ramsamooj <shiva.ramsamooj@hotmail.com>
2019-04-11 01:08:28 -0400

    began blogs app, routed urls for home view

--- joey/urls.py
+++ joey/urls.py
@@ -20,5 +20,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
+    path('blog/', include('blog.urls')),
     path('admin/', admin.site.urls),
 ]
