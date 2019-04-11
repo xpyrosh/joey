7c8abaec16d80e2512aa1351ea09b96b129ff76d
Shiva Ramsamooj <shiva.ramsamooj@hotmail.com>
2019-04-11 01:42:29 -0400

    changed blog/views to use html templates also created dummy post data dictionary to use

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 app_name = 'blog'
 urlpatterns = [
-    path('', views.home, name='blog-home')
+    path('', views.home, name='blog-home'),
+    path('about/', views.about, name='blog-about'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,27 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+posts = [
+    {
+        'author': 'Shiva',
+        'title': 'Blog Post 1',
+        'content': 'First Post Content',
+        'date_posted': 'April 10, 2019'
+    },
+    {
+        'author': 'Not Shiva',
+        'title': 'Blog Post 2',
+        'content': 'Second Post Content',
+        'date_posted': 'April 11, 2019'
+    }
+]
 
+
+# home page view
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    return render(request, 'blog/home.html')
+
+
+# about page view
+def about(request):
+    return render(request, 'blog/about.html')
