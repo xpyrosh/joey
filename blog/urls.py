52306ce0952c14accc5fea9b8b9babf6934b950f
Shiva Ramsamooj <shiva.ramsamooj@hotmail.com>
2019-04-11 03:52:41 -0400

    removed app_name='blog' from urls.py because it was causing a crash in base.html when using {% url 'blog-home' %} instead of '/blog' for the <href> I don't understand why this is an issue

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from . import views
 
-app_name = 'blog'
+# app_name = 'blog'
 urlpatterns = [
     path('', views.home, name='blog-home'),
     path('about/', views.about, name='blog-about'),
