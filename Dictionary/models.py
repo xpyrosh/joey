aaddd82e0ea85fed39a2257c0fca11d12529d1aa
Shiva Ramsamooj <shiva.ramsamooj@hotmail.com>
2019-04-12 15:34:24 -0400

    added __str__ methods to my models added models to admin page

--- Dictionary/models.py
+++ Dictionary/models.py
@@ -10,6 +10,9 @@ from django.contrib.auth.models import User
 class Word (models.Model):
     word_text = models.CharField(max_length=100, unique=True)
 
+    def __str__(self):
+        return self.word_text
+
 
 class Desc(models.Model):
     # creates the one to many relation to word
@@ -22,4 +25,4 @@ class Desc(models.Model):
     posted_by = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.word_text
+        return self.desc_text
