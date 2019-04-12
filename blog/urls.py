6c94e9c06686866691e58eb6c4fff23520723bc8
Shiva Ramsamooj <shiva.ramsamooj@hotmail.com>
2019-04-11 20:31:54 -0400

    fixed previous urltag error when using app_name = blog

--- blog/urls.py
+++ blog/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 
 from . import views
 
-# app_name = 'blog'
+app_name = 'blog'
 urlpatterns = [
-    path('', views.home, name='blog-home'),
-    path('about/', views.about, name='blog-about'),
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
 ]
