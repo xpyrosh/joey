b02cd8ad85badba092c06b7b289725e6cc9dd194
Shiva Ramsamooj <shiva.ramsamooj@hotmail.com>
2019-04-14 02:21:07 -0400

    Created a search form for testing implemented simple search on home page to search for word name

--- Dictionary/views.py
+++ Dictionary/views.py
@@ -8,5 +8,26 @@ def index(request):
         'words': Word.objects.all(),
         'desc': Desc.objects.all()
     }
+    if request.method == 'POST':
+        search_id = request.POST.get('title', None)
+        try:
+            word = Word.objects.get(word_text=search_id)
+            context['words'] = [word]
+            print(context['words'])
+        except Word.DoesNotExist:
+            return HttpResponse("Word does not exist.")
+
     return render(request, 'Dictionary/home.html', context)
 
+
+    # def search(request):
+    #     if request.method == 'POST':
+    #         search_id = request.POST.get('textfield', None)
+    #         try:
+    #             word = Word.objects.get(word_text = search_id)
+    #             html = ("<h1>%s<h1/>", word)
+    #             return HttpResponse(html)
+    #         except Word.DoesNotExist:
+    #             return HttpResponse("Word does not exist.")
+    #     else:
+    #         return render(request, 'Dictionary/search.html')
