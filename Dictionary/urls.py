5af3a12849d72861ed9ffc00bf2c31fcc629b7cb
Shiva Ramsamooj <shiva.ramsamooj@hotmail.com>
2019-04-21 04:26:06 -0400

    added login url and login html, currently not functioning

--- Dictionary/urls.py
+++ Dictionary/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-
+from django.contrib.auth.views import LoginView
 from . import views
 
 app_name = 'UDictionary'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('login/', LoginView.as_view(template_name='Dictionary/login.html')),
     path('addword/', views.addword, name='addword'),
     path('results/', views.search, name='search'),
     path('results/<str:search_id>/', views.results, name='results')
