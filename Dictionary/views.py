23f8d7fdec34a509a54f13534cd15e4ffc9b8ca4
Shiva Ramsamooj <shiva.ramsamooj@hotmail.com>
2019-04-24 15:42:38 -0400

    registration page works and sucessfully adds new user to database added to base.html message alerts to show user was sucessfully created

--- Dictionary/views.py
+++ Dictionary/views.py
@@ -3,6 +3,7 @@ from .models import Word, Desc
 from django.contrib.auth.models import User
 from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 context = {
         'words': Word.objects.all(),
@@ -75,7 +76,16 @@ def login(request):
 
 
 def register(request):
-    form = UserCreationForm()
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            form.save()
+            return redirect('UDictionary:login')
+    else:
+        form = UserCreationForm()
+
     context = {
         'form': form
     }
