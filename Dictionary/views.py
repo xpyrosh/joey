3a9f90ce5dc08c2ae8e518c3c53bdae11478c54e
Shiva Ramsamooj <shiva.ramsamooj@hotmail.com>
2019-04-24 23:41:52 -0400

    changed nav bar to handled logged in users by removing login and register option as well as providing visual feedback and logout option for a logged in user

--- Dictionary/views.py
+++ Dictionary/views.py
@@ -16,20 +16,6 @@ def index(request):
         'words': Word.objects.all(),
         'desc': Desc.objects.all()
     }
-    # if request.method == 'POST':
-    #     search_id = request.POST.get('title', None)
-    #     context['search_id'] = search_id
-    #     try:
-    #         word = Word.objects.filter(word_text__icontains=search_id)
-    #         context['words'] = word
-    #         if not word:
-    #             word = Desc.objects.filter(desc_text__icontains=search_id)
-    #             context['words'] = []
-    #             for w in word:
-    #                 context['words'].append(w.word)
-    #
-    #     except Word.DoesNotExist:
-    #         return HttpResponse("Word does not exist.")
 
     return render(request, 'Dictionary/home.html', context)
 
